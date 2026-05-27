264eff5d7fa42623aae68211ead1b56b95ac596c
Seyongski <jsy010618@gmail.com>
2026-05-27 10:29:14 +0900

    애니메이션
    애니메이션데이터에셋과 에니메이션 컴포넌트 클래스 추가

---
+++ SlayTheChampions/Source/SlayTheChampions/Public/Unit/UnitAnimDataAsset.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "UnitAnimDataAsset.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SLAYTHECHAMPIONS_API UUnitAnimDataAsset : public UPrimaryDataAsset
+{
+	GENERATED_BODY()
+	
+};
